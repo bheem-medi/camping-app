96a80b6385e7644c405b3beb6fedea3f248c68a1
Bheem Medi <eeswar887@gmail.com>
2026-03-31 10:42:40 +0530

    Delete backend/src/controller directory

--- backend/src/controller/PackageController.java
+++
@@ -1,9 +0,0 @@
-package com.camping.repository;
-
-import com.camping.model.CampingPackage;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
-
-public interface PackageRepository extends MongoRepository<CampingPackage, String> {
-    List<CampingPackage> findByLocationContaining(String location);
-}
